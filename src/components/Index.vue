98620be7663c19de48cef25bd73264d0c67b00ea
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-04-01 18:51:01 +0900

    add: ストップ、ボタン切り替え機能

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,10 +2,14 @@
   <div>
     <p>{{ frontTitle }}</p>の
     <p>{{ backTitle }}</p>
-    <button @click="changeFrontTitle()">押せ！1</button>
+
+    <button v-if="hoge" @click="changeFrontTitle()">押せ！1</button>
+    <button v-else @click="stop()">ストップ</button>
+
     <button @click="changeBackTitle()">押せ！2</button>
-    <a href="https://twitter.com/intent/tweet?text=ツイート本文
-">押してね</a>
+
+    <!-- v-ifで見えなくする -->
+    <a @click="makeTweet">ツイートする</a>
   </div>
 </template>
 
@@ -16,7 +20,12 @@ export default {
       frontTitle: "境界面上",
       backTitle: "シュタインズゲート",
       frontWords: ["永劫回帰", "支離滅裂", "輪廻転生", "晴耕雨読"],
-      backWords: ["エムブレム", "ドリーム", "ブリザード", "ニライカナイ"]
+      backWords: ["エムブレム", "ドリーム", "ブリザード", "ニライカナイ"],
+      hoge: true,
+      aaa: "",
+
+      createdUrl: "",
+      tweetDefault: "https://twitter.com/intent/tweet?text="
     };
   },
   name: "Index",
@@ -35,15 +44,22 @@ export default {
       ];
     },
     changeFrontTitle: function() {
-      setInterval(() => {
+      this.aaa = setInterval(() => {
         this.randomFrontTitle();
       }, 100);
+      this.hoge = !this.hoge;
     },
     changeBackTitle: function() {
       setInterval(() => {
         this.randomBackTitle();
       }, 100);
     },
+    makeTweet: function() {},
+    stop: function() {
+      console.log("test");
+      clearInterval(this.aaa);
+      this.hoge = !this.hoge;
+    }
   }
 };
 </script>
